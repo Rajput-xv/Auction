8c40251e80fae162043a13cf56fcee3c4fa2c90e
Yash Verma <114978813+Rajput-xv@users.noreply.github.com>
2025-04-20 12:30:41 +0530

    initial

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {
+	registerUser,
+	loginUser,
+	getProfile,
+	logoutUser,
+} = require("../controllers/userController");
+const router = express.Router();
+const { authMiddleware } = require("../middleware/authMiddleware");
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/profile",authMiddleware, getProfile);
+router.post("/logout", logoutUser);
+
+module.exports = router;
