591e42ec91e308052a453048cb523e689d08e962
Yash Verma <114978813+Rajput-xv@users.noreply.github.com>
2025-04-20 13:00:37 +0530

    update url

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// const target = "https://auction-system-server.onrender.com/api"; //  backend server URL
-const target = "http://localhost:5000/api"; //  backend server URL
+const target = "https://auction-8s0d.onrender.com/api"; //  backend server URL
+// const target = "http://localhost:5000/api"; //  backend server URL
 
 export default defineConfig({
 	plugins: [react()],
--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: ["https://auction-system-client.vercel.app", "http://localhost:5173"],
+		origin: ["https://auction-lac.vercel.app/", "http://localhost:5173"],
 		methods: ["GET", "PUT", "POST", "DELETE"],
 		credentials: true,
 	})
