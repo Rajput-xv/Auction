b42c92786e8ab96e36161131f7669594aad38ef1
Yash Verma <114978813+Rajput-xv@users.noreply.github.com>
2025-09-10 11:42:56 +0530

    update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,13 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
 	try {
-		// Using mongoose connection options instead of deprecated setting
+		// Setting strictQuery before connection
+		mongoose.set('strictQuery', false);
+		
 		const conn = await mongoose.connect(process.env.MONGODB_URI, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
 			serverSelectionTimeoutMS: 5000, // Timeout after 5s
 			socketTimeoutMS: 45000, // Close sockets after 45s of inactivity
-			strictQuery: false // Set through options instead of global setting
 		});
 		console.log(`MongoDB Connected: ${conn.connection.host}`);
 	} catch (error) {
